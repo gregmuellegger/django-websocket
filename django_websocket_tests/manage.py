11f4ab2a0c7d95b5a7a86c276d6306a324c94981
Gregor Müllegger <gregor@muellegger.de>
2010-07-17 13:48:29 +0200

    Reverting some changes in manage.py

--- django_websocket_tests/manage.py
+++ django_websocket_tests/manage.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 import os
 import sys
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
+sys.path.insert(0,
+os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from django.core.management import execute_manager
 try:
-    import django_websocket
+    import settings # Assumed to be in the same directory.
 except ImportError:
-    sys.path.insert(0, os.path.dirname(
-        os.path.dirname(os.path.abspath(__file__))))
-    try:
-        import django_websocket
-    except ImportError:
-        print "Cannot find a distribution of django_websocket."
-        sys.exit(1)
-
-from django.core import management
+    import sys
+    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.exit(1)
 
 if __name__ == "__main__":
-    management.execute_from_command_line()
+    execute_manager(settings)
