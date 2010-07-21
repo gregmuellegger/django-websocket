44c56d93ec3cb8a72e17189e986b100bde59bfda
Gregor Müllegger <gregor@muellegger.de>
2010-07-21 18:17:53 +0200

    Fixing issue with tests in django 1.1

--- django_websocket_tests/runtests.py
+++ django_websocket_tests/runtests.py
@@ -4,18 +4,19 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'django_websocket_tests.settings'
 test_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, test_dir)
 
+import django
 from django.test.utils import get_runner
 from django.conf import settings
 
 def runtests():
     TestRunner = get_runner(settings)
-    if hasattr(TestRunner, '__class__'):
+    if django.VERSION[:2] > (1,1):
+        test_runner = TestRunner(verbosity=1, interactive=True)
+        failures = test_runner.run_tests(settings.TEST_APPS)
+    else:
         # test runner is not class based, this means we use django 1.1.x or
         # earlier.
         failures = TestRunner(settings.TEST_APPS, verbosity=1, interactive=True)
-    else:
-        test_runner = TestRunner(verbosity=1, interactive=True)
-        failures = test_runner.run_tests(settings.TEST_APPS)
     sys.exit(bool(failures))
 
 if __name__ == '__main__':
